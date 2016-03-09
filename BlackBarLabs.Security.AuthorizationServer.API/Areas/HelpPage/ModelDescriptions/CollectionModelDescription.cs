7a6e11cc62625372f6c0419e7ff3446bd35d025f
joshcodes <josh@blackbarlabs.com>
2016-03-08 19:01:59 -0600

    moving

--- BlackBarLabs.Security.AuthorizationServer.API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace BlackBarLabs.Security.AuthorizationServer.API.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- BlackBarLabs.Security.AuthorizationServer.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace BlackBarLabs.Security.AuthorizationServer.API.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
